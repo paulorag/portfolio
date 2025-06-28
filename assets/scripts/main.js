1b40009f29ccc77543c75b79f39133b577b2cac9
Paulo Roberto <devpaulorag@gmail.com>
2025-06-28 15:51:28 -0300

    Refatora a estrutura do HTML e ajustes no script main.js, themeToggle.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,8 @@
+// main.js
 import { initSmoothScroll } from "./smoothScroll.js";
 import { initMobileMenu } from "./menuMobile.js";
 import { initBackToTop } from "./backToTopButton.js";
-import { initThemeToggle } from "./themeToggle.js";
+import { initThemeToggle } from "./themeToggle.js"; // Importando o tema
 
 document.addEventListener("DOMContentLoaded", () => {
     initThemeToggle();
--- assets/scripts/themeToggle.js
+++ assets/scripts/themeToggle.js
@@ -1,23 +1,43 @@
+// themeToggle.js
+
 export function initThemeToggle() {
     const toggleBtn = document.getElementById("themeToggle");
 
     if (!toggleBtn) return;
 
-    // Aplica o tema salvo (se houver)
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme) {
-        document.documentElement.setAttribute("data-theme", savedTheme);
-    } else {
-        // Se o tema não estiver salvo, verifica a preferência do sistema
-        const prefersDarkScheme = window.matchMedia(
-            "(prefers-color-scheme: dark)"
-        ).matches;
-        document.documentElement.setAttribute(
-            "data-theme",
-            prefersDarkScheme ? "dark" : "light"
-        );
-    }
+    // Função para atualizar o tema com base nas preferências do usuário ou do sistema
+    const updateTheme = () => {
+        const savedTheme = localStorage.getItem("theme");
 
+        if (savedTheme) {
+            document.documentElement.setAttribute("data-theme", savedTheme); // Aplica o tema salvo
+        } else {
+            // Verifica a preferência do sistema (dark ou light)
+            const prefersDarkScheme = window.matchMedia(
+                "(prefers-color-scheme: dark)"
+            ).matches;
+            document.documentElement.setAttribute(
+                "data-theme",
+                prefersDarkScheme ? "dark" : "light"
+            );
+        }
+    };
+
+    // Aplica o tema ao carregar a página
+    updateTheme();
+
+    // Verifica se há alteração nas preferências de tema do sistema
+    window
+        .matchMedia("(prefers-color-scheme: dark)")
+        .addEventListener("change", () => {
+            // Se o tema não foi manualmente salvo, atualiza com base nas preferências do sistema
+            const savedTheme = localStorage.getItem("theme");
+            if (!savedTheme) {
+                updateTheme();
+            }
+        });
+
+    // Alterna o tema quando o botão é clicado
     toggleBtn.addEventListener("click", () => {
         const current = document.documentElement.getAttribute("data-theme");
         const newTheme = current === "dark" ? "light" : "dark";
