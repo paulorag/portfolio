0835e40c3428dfa96ae54b9752003ec7eeda050b
Paulo Roberto <devpaulorag@gmail.com>
2025-06-28 16:56:10 -0300

    Ajusta cores de seções e fundos

--- assets/scripts/functions/themeToggle.js
+++ assets/scripts/functions/themeToggle.js
@@ -3,44 +3,36 @@ export function initThemeToggle() {
 
     if (!toggleBtn) return;
 
-    // Função para atualizar o tema com base nas preferências do usuário ou do sistema
     const updateTheme = () => {
         const savedTheme = localStorage.getItem("theme");
 
         if (savedTheme) {
-            // Aplica o tema salvo pelo usuário
             document.documentElement.setAttribute("data-theme", savedTheme);
         } else {
-            // Caso não tenha tema salvo, verifica as preferências do sistema
             const prefersDarkScheme = window.matchMedia(
                 "(prefers-color-scheme: dark)"
             ).matches;
-            // Define o tema de acordo com as preferências do sistema
             const theme = prefersDarkScheme ? "dark" : "light";
             document.documentElement.setAttribute("data-theme", theme);
         }
     };
 
-    // Aplica o tema ao carregar a página
     updateTheme();
 
-    // Verifica alterações nas preferências do sistema
     window
         .matchMedia("(prefers-color-scheme: dark)")
         .addEventListener("change", () => {
-            // Verifica se o tema não foi definido manualmente pelo usuário
             const savedTheme = localStorage.getItem("theme");
             if (!savedTheme) {
                 updateTheme();
             }
         });
 
-    // Alterna o tema quando o botão é clicado
     toggleBtn.addEventListener("click", () => {
         const current = document.documentElement.getAttribute("data-theme");
         const newTheme = current === "dark" ? "light" : "dark";
 
         document.documentElement.setAttribute("data-theme", newTheme);
-        localStorage.setItem("theme", newTheme); // Salva o tema selecionado pelo usuário
+        localStorage.setItem("theme", newTheme);
     });
 }
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,8 +1,7 @@
-// main.js
 import { initSmoothScroll } from "./functions/smoothScroll.js";
 import { initMobileMenu } from "./functions/menuMobile.js";
 import { initBackToTop } from "./functions/backToTopButton.js";
-import { initThemeToggle } from "./functions/themeToggle.js"; // Importando o tema
+import { initThemeToggle } from "./functions/themeToggle.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     initThemeToggle();
